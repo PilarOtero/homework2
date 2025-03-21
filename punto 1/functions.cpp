b86d981f7726f4f9ca8effe79fa5be7c6b6016da
Pilar Otero <potero@udesa.edu.ar>
2025-03-20 21:10:59 -0300

    comienzo creacion clase time

---
+++ punto 1/functions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Time {
+    private:
+        int hour;
+        int minutes;
+        int seconds;
+        string am_pm;
+
+
+};
+
+
+
